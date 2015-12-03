21ac7d7daa190aa9f453cb34d06060ba895bb1c8
RamseyDarling <rdarling42@gmail.com>
2015-12-02 22:31:21 -0500

    setting up DB class for postgresql

--- Db.php
+++ Db.php
@@ -11,26 +11,40 @@ class Db {
     private $user;
     private $host;
     private $pass;
+    private $connection_string;
     
     public function __construct($env='dev'){
         switch($env){            
             case 'dev':
                 $this->set_dev_db();
+                $this->set_connection_string();
                 break;
         }
     }
     
     public function set_dev_db(){
-        $this->name = '';
-        $this->user = '';
-        $this->host = '';
-        $this->pass = '';
+        $this->name = 'mydb';
+        $this->user = 'postgres';
+        $this->host = 'localhost';
+        $this->pass = 'letmein42';
+    }
+
+    public function set_connection_string($type='postgresql'){
+        switch($type){
+        case 'mysql':
+            $this->connection_string = "'mysql:host='.$this->host.';dbname='.$this->name,$this->user,$this->pass";
+            break;
+        case 'postgresql':
+            //$this->connection_string = "pgsql:host={$this->host};port=5432;dbname={$this->name};user={$this->user};password={$this->pass}";
+            $this->connection_string = "pgsql:user={$this->user};dbname={$this->name};password={$this->pass}";
+            break;
+        }
     }
     
     public function select_query($query,$ex_data=null){
         //most generic query. just takes in an sql query
         try{
-            $conn = new PDO('mysql:host='.$this->host.';dbname='.$this->name,$this->user,$this->pass);
+            $conn = new PDO($this->connection_string);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //set error mode to fetch error messages
             $qu = $query;
             $act = $conn->prepare($qu);
@@ -49,7 +63,7 @@ class Db {
     public function select($table_name, $ex_data = null, $where = null, $and = null){
         //database helper method to get stuff out of the db
         try{
-            $conn = new PDO('mysql:host='.$this->host.';dbname='.$this->name,$this->user,$this->pass);
+            $conn = new PDO($this->connection_string);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //set error mode to fetch error messages
             $qu = "SELECT * FROM {$table_name}";
             if($where != null){
@@ -75,7 +89,7 @@ class Db {
         //database helper method to get stuff out of the db
         //allows for particular columns to be selected.
         try{
-            $conn = new PDO('mysql:host='.$this->host.';dbname='.$this->name,$this->user,$this->pass);
+            $conn = new PDO($this->connection_string);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //set error mode to fetch error messages
             
             $qu = "SELECT {$col_name} FROM {$table_name}";
@@ -106,7 +120,7 @@ class Db {
     public function insert($table_name,$col_names,$values,$ex_data){
         //database helper method, controls inserts
         try {
-            $conn = new PDO('mysql:host='.$this->host.';dbname='.$this->name,$this->user,$this->pass);
+            $conn = new PDO($this->connection_string);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $qu = "INSERT INTO {$table_name} ({$col_names}) VALUES({$values})";
             $act = $conn->prepare($qu);
@@ -123,7 +137,7 @@ class Db {
     public function select_order_by($table_name, $orderby, $sort, $limit=null){
         //database helper method to get stuff out of the db
         try{
-            $conn = new PDO('mysql:host='.$this->host.';dbname='.$this->name,$this->user,$this->pass);
+            $conn = new PDO($this->connection_string);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //set error mode to fetch error messages
             
             $qu = "SELECT * FROM {$table_name} ORDER BY {$orderby} {$sort}";
@@ -139,9 +153,7 @@ class Db {
             $res = $act->fetchAll();
             
         }catch(PDOException $e){
-            //not going to do anything with the error message
-            //right now
-            $res = false;
+            $res = $e;//set to false for production, $e for debugging
         }
 
         return $res; 
@@ -149,7 +161,7 @@ class Db {
 
     public function select_where_order_by($table_name, $where, $orderby, $ex_data, $sort=null, $limit=null){
         try{
-            $conn = new PDO('mysql:host='.$this->host.';dbname='.$this->name,$this->user,$this->pass);
+            $conn = new PDO($this->connection_string);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //set error mode to fetch error messages
             
             $qu = "SELECT * FROM {$table_name} WHERE {$where} ORDER BY {$orderby}";
--- index.php
+++ index.php
@@ -10,7 +10,10 @@ include_once 'Db.php';
 
 $db = new Db();
 
-$rows = $db->select_order_by('wonderbug','one','DESC','100');
+$rows = $db->select_order_by('item','id','DESC','100');
+
+var_dump($rows);
+die;
 
 if(!empty($rows)){
     $csv = "Handle,Title,Body (HTML),Vendor,Type,Tags,Published,Option1 Name,Option1 Value,Option2 Name,Option2 Value,Option3 Name,Option3 Value,Variant SKU,Variant Grams,Variant Inventory Tracker,Variant Inventory Qty,Variant Inventory Policy,Variant Fulfillment Service,Variant Price,Variant Compare At Price,Variant Requires Shipping,Variant Taxable,Variant Barcode,Image Src,Image Alt Text,Gift Card,Google Shopping / MPN,Google Shopping / Age Group,Google Shopping / Gender,Google Shopping / Google Product Category,SEO Title,SEO Description,Google Shopping / AdWords Grouping,Google Shopping / AdWords Labels,Google Shopping / Condition,Google Shopping / Custom Product,Google Shopping / Custom Label 0,Google Shopping / Custom Label 1,Google Shopping / Custom Label 2,Google Shopping / Custom Label 3,Google Shopping / Custom Label 4,Variant Image,Variant Weight Unit\n";
