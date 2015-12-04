00502dfd9d6d93718e885dede6682f4e9c21b977
RamseyDarling <rdarling42@gmail.com>
2015-12-03 21:55:11 -0500

    successfully creating CSV files

--- index.php
+++ index.php
@@ -12,20 +12,18 @@ $db = new Db();
 
 $rows = $db->select_order_by('item','id','DESC','100');
 
-var_dump($rows);
-die;
 
 if(!empty($rows)){
     $csv = "Handle,Title,Body (HTML),Vendor,Type,Tags,Published,Option1 Name,Option1 Value,Option2 Name,Option2 Value,Option3 Name,Option3 Value,Variant SKU,Variant Grams,Variant Inventory Tracker,Variant Inventory Qty,Variant Inventory Policy,Variant Fulfillment Service,Variant Price,Variant Compare At Price,Variant Requires Shipping,Variant Taxable,Variant Barcode,Image Src,Image Alt Text,Gift Card,Google Shopping / MPN,Google Shopping / Age Group,Google Shopping / Gender,Google Shopping / Google Product Category,SEO Title,SEO Description,Google Shopping / AdWords Grouping,Google Shopping / AdWords Labels,Google Shopping / Condition,Google Shopping / Custom Product,Google Shopping / Custom Label 0,Google Shopping / Custom Label 1,Google Shopping / Custom Label 2,Google Shopping / Custom Label 3,Google Shopping / Custom Label 4,Variant Image,Variant Weight Unit\n";
     foreach($rows as $row){
         //declairing variables to prevent array item not set errors at runtime
-        $handle = !empty($row['handle']) ? $row['handle'] : null;
-        $title = !empty($row['title']) ? $row['title'] : null;
-        $body = !empty($row['body']) ? $row['body'] : null;
-        $vendor = !empty($row['vendor']) ? $row['vendor'] : null;
-        $type = !empty($row['type']) ? $row['type'] : null;
+        $handle = !empty($row['id']) ? $row['id'] : null;
+        $title = !empty($row['description']) ? $row['description'] : null;
+        $body = !empty($row['copy']) ? $row['copy'] : null;
+        $vendor = !empty($row['vendor_id']) ? $row['vendor_id'] : null;
+        $type = !empty($row['dept_id']) ? $row['dept_id'] : null;
         $tags = !empty($row['tags']) ? $row['tags'] : null;
-        $published = !empty($row['published']) ? $row['published'] : null;
+        $published = !empty($row['active']) ? $row['active'] : null;
         $option1_name = !empty($row['option1_name']) ? $row['option1_name'] : null;
         $option1_value = !empty($row['option1_value']) ? $row['option1_value'] : null;
         $option2_name = !empty($row['option2_name']) ? $row['option2_name'] : null;
@@ -35,14 +33,14 @@ if(!empty($rows)){
         $variant_sku = !empty($row['variant_sku']) ? $row['variant_sku'] : null;
         $variant_grams = !empty($row['variant_grams']) ? $row['variant_grams'] : null;
         $variant_inventory_tracker = !empty($row['placeholder']) ? $row['placeholder'] : null;
-        $varient_inventory_qty = !empty($row['varient_inventory_qty']) ? $row['varient_inventory_qty'] : null;
+        $varient_inventory_qty = !empty($row['qty']) ? $row['qty'] : null;
         $variant_inventory_policy = !empty($row['placeholder']) ? $row['placeholder'] : null;
         $variant_fulfillment_service = !empty($row['placeholder']) ? $row['placeholder'] : null;
-        $variant_price = !empty($row['placeholder']) ? $row['placeholder'] : null;
+        $variant_price = !empty($row['cost']) ? $row['cost'] : null;
         $variant_compaire_at_price = !empty($row['placeholder']) ? $row['placeholder'] : null;
         $variant_requires_shipping = !empty($row['placeholder']) ? $row['placeholder'] : null;
         $variant_taxable = !empty($row['placeholder']) ? $row['placeholder'] : null;
-        $variant_barcode = !empty($row['placeholder']) ? $row['placeholder'] : null;
+        $variant_barcode = !empty($row['upccode']) ? $row['upccode'] : null;
         $image_src = !empty($row['placeholder']) ? $row['placeholder'] : null;
         $image_alt_text = !empty($row['placeholder']) ? $row['placeholder'] : null;
         $gift_card = !empty($row['placeholder']) ? $row['placeholder'] : null;
@@ -50,8 +48,8 @@ if(!empty($rows)){
         $age_group_google_shopping = !empty($row['placeholder']) ? $row['placeholder'] : null;
         $gender_google_shopping = !empty($row['placeholder']) ? $row['placeholder'] : null;
         $google_product_category = !empty($row['placeholder']) ? $row['placeholder'] : null;
-        $seo_title = !empty($row['placeholder']) ? $row['placeholder'] : null;
-        $seo_description = !empty($row['placeholder']) ? $row['placeholder'] : null;
+        $seo_title = !empty($row['description']) ? $row['description'] : null;
+        $seo_description = !empty($row['description']) ? $row['description'] : null;
         $google_shopping_adwords_grouping = !empty($row['placeholder']) ? $row['placeholder'] : null;
         $google_shopping_adwords_labels = !empty($row['placeholder']) ? $row['placeholder'] : null;
         $google_shopping_condition = !empty($row['placeholder']) ? $row['placeholder'] : null;
