7013d38adc3f8031116d4ccdc4ef2761934f38d3
RamseyDarling <rdarling42@gmail.com>
2016-01-05 20:54:19 -0500

    adding gitignore, adding a function to remove quotes from html body content in body field

--- index.php
+++ index.php
@@ -8,6 +8,11 @@
 
 include_once 'Db.php';
 
+function remove_quotes($str){
+    $str = str_replace('"','',$str);
+    return $str;
+}
+
 $db = new Db();
 
 $rows = $db->select_order_by('item','id','DESC','100');
@@ -16,6 +21,7 @@ $rows = $db->select_order_by('item','id','DESC','100');
 if(!empty($rows)){
     $csv = "Handle,Title,Body (HTML),Vendor,Type,Tags,Published,Option1 Name,Option1 Value,Option2 Name,Option2 Value,Option3 Name,Option3 Value,Variant SKU,Variant Grams,Variant Inventory Tracker,Variant Inventory Qty,Variant Inventory Policy,Variant Fulfillment Service,Variant Price,Variant Compare At Price,Variant Requires Shipping,Variant Taxable,Variant Barcode,Image Src,Image Alt Text,Gift Card,Google Shopping / MPN,Google Shopping / Age Group,Google Shopping / Gender,Google Shopping / Google Product Category,SEO Title,SEO Description,Google Shopping / AdWords Grouping,Google Shopping / AdWords Labels,Google Shopping / Condition,Google Shopping / Custom Product,Google Shopping / Custom Label 0,Google Shopping / Custom Label 1,Google Shopping / Custom Label 2,Google Shopping / Custom Label 3,Google Shopping / Custom Label 4,Variant Image,Variant Weight Unit\n";
     foreach($rows as $row){
+        $row['copy'] = remove_quotes($row['copy']);
         //declairing variables to prevent array item not set errors at runtime
         $handle = !empty($row['id']) ? $row['id'] : null;
         $title = !empty($row['description']) ? $row['description'] : null;
@@ -93,3 +99,4 @@ if(!empty($rows)){
     //exit the script with warning message
     die("Sorry, there was an error grabbing the data from database\n");
 }
+
