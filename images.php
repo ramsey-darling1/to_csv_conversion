c2fda345c451344192e585746e8459d5e01c59b0
RamseyDarling <rdarling42@gmail.com>
2016-01-16 14:54:21 -0500

    increased count to 500

--- images.php
+++ images.php
@@ -10,18 +10,22 @@ include_once 'Db.php';
 
 $db = new Db();
 
-$rows = $db->select_order_by('images','id','DESC','100');
+$rows = $db->select_order_by('images','id','DESC',500);
 
 $count = 0;
 
 if(!empty($rows)){
     echo "Pulling images out of database...\n";
     foreach($rows as $row){
-        $image = imagecreatefromstring(stream_get_contents($row['image']));
-        imagejpeg($image, "images/{$row['item_id']}.jpg");
-        echo "Created image {$count}\n";
+        if(!empty($row['image']) && !empty($row['item_id'])){
+            $image = imagecreatefromstring(stream_get_contents($row['image']));
+            imagejpeg($image, "images/{$row['item_id']}.jpg");
+            echo "Created image {$count}\n";
+            $last_image_id = $row['id'];
+        }
         $count += 1;
     }
+    echo "Last image ID created {$last_image_id}\n";
 }else{
     echo "No images found in database\n";
 }
